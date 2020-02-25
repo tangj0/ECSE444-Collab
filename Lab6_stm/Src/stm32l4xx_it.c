fda69341be10792edc28eed38a266ddcd6be2b5e
tangj0 <jtang186@gmail.com>
2020-02-25 11:15:56 -0500

    comment out systick interrupt

--- Lab6_stm/Src/stm32l4xx_it.c
+++ Lab6_stm/Src/stm32l4xx_it.c
@@ -177,8 +177,8 @@ void SysTick_Handler(void) //stops working without this handler
   /* USER CODE BEGIN SysTick_IRQn 0 */
 
   /* USER CODE END SysTick_IRQn 0 */
-  HAL_IncTick();
-  HAL_SYSTICK_IRQHandler();
+  HAL_IncTick(); //need this to drive program
+  //HAL_SYSTICK_IRQHandler();
   /* USER CODE BEGIN SysTick_IRQn 1 */
 
   /* USER CODE END SysTick_IRQn 1 */
